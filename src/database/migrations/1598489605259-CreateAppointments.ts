ba998916c2bc7ab44f669a2517354217ffaae525
Anderson Campos <anderleitec@gmail.com>
2020-08-26 23:13:21 -0300

    TypeORM repository

--- src/database/migrations/1598489605259-CreateAppointments.ts
+++ src/database/migrations/1598489605259-CreateAppointments.ts
@@ -12,6 +12,7 @@ export default class CreateAppointments1598489605259
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
