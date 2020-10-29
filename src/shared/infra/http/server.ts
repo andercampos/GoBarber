0b2f3ef69a7fcf89ff95d22e456d2c13df7a34e7
Anderson Campos <anderleitec@gmail.com>
2020-10-28 23:34:31 -0300

    Tokens migration & password routes

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -35,5 +35,5 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
 });
 
 app.listen(3333, () => {
-  console.log('✔ Server started on port 3333!');
+  console.log('🚀 Server started on port 3333!');
 });
